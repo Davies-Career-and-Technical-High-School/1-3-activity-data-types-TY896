d7245822b1fd2adfced75fbb294a009278d2b7df
TYLER BROOKS <tyler.brooks@daviestech.org>
2020-10-02 15:48:38 +0000

    everything

--- Question2.java
+++ Question2.java
@@ -1,9 +1,15 @@
-
+import java.util.Scanner;
 
 class Question2 {
     public static void main(String[] args) {
-
+     Scanner scan = new Scanner(System.in);
         /* Write your code here */
-
+     System.out.println(" hi there pal What is your name?");
+     System.out.println(" how old are you?");
+     String name;
+     String age;
+     name = scan.nextLine();
+      age = scan.nextLine();
+      System.out.println("Hello " + name + "is" + " " + age + "  ");
     }
 }
