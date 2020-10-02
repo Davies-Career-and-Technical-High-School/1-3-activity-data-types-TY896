4497a63fe194467e532d7cfc59544abfa5b8c798
TYLER BROOKS <tyler.brooks@daviestech.org>
2020-10-02 15:38:54 +0000

    everthing

--- Question1.java
+++ Question1.java
@@ -1,9 +1,15 @@
-
+import java.util.Scanner;
 
 class Question1 {
     public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
 
         /* Write your code here */
-
+      double number1 = scan.nextDouble();
+      
+      double number2 = scan.nextDouble();
+      System.out.print(number2 + " " + " " + number1 + "  " );
+      
+      
     }
 }
